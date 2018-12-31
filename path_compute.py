fa5b07bdebd07e8d5fe57c7c0454174909a99a7f
watal <watal.i27e@gmail.com>
2018-12-31 19:38:45 +0900

    Fix variable name

--- path_compute.py
+++ path_compute.py
@@ -6,8 +6,9 @@ import json
 import pce_segmentlist.py
 
 
-def cspf():
+def cspf(addr):
     '''Constrained Shortest Path First'''
+
     with open('segment_lists.json') as f:
         segment_lists = json.load(f)
 
@@ -17,7 +18,7 @@ def cspf():
 def compute(addr):
     '''computing cspf'''
 
-
-        pce_segmentlist.ssocket(addr, segment_lists)
+    segment_lists = cspf(addr)
+    pce_segmentlist.ssocket(addr, segment_lists)
 
     return
